fe1ea179ac7bc9b46302a4ec59759bb6eb9d8604
dylanshah <50290719+dylanshah@users.noreply.github.com>
2020-07-01 09:13:47 +0300

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,5 @@
+//To connect to the database
+<?php
+    $db = mysqli_connect('localhost','dcarryc1_admin','Simb@020401','dcarryc1_Items')
+    or die('Error connecting to MySQL server.');
+?>
---
+++ product.js
@@ -0,0 +1,4 @@
+//Javascript function to get which item is selected from the dropdown menu 
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
